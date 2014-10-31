65579c23260328f57f706e9e0b6c7fd36434f226
Rick <rickliao@utexas.edu>
2014-10-31 18:50:32 -0500

    test

---
+++ False.scala
@@ -0,0 +1,12 @@
+class False {
+	def +(a: Int) {
+		println(a);
+	}
+	
+}
+object False{
+def main(args: Array[String]) {
+		var obj :False= new False();
+		obj.+ (1);
+	}}
+//https://github.com/RyanHolstien/False
